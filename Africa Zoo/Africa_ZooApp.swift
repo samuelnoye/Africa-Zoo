1cd5f09050372c204f0a2a29e83d296b1e2704b2
Samuel Noye <samuel.noye@amalitech.org>
2021-12-11 15:10:33 +0000

    Initial Commit

---
+++ Africa Zoo/Africa_ZooApp.swift
@@ -0,0 +1,17 @@
+//
+//  Africa_ZooApp.swift
+//  Africa Zoo
+//
+//  Created by Samuel Noye on 11/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct Africa_ZooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Africa Zoo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Africa Zoo
+//
+//  Created by Samuel Noye on 11/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
