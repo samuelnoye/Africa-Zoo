187f24746aefe8a06dfe732db07a56052c76fd79
Samuel Noye <samuel.noye@amalitech.org>
2022-05-12 17:47:18 +0000

    Update ContentView.swift

--- Africa Zoo/ContentView.swift
+++ Africa Zoo/ContentView.swift
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
+// struct ContentView: View {
+//     var body: some View {
+//         Text("Hello, world!")
+//             .padding()
+//     }
+// }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
